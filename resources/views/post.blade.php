8063391704678ddf96ace4ca309dbc148a42427e
DinoLadavac <ladavac.dino@gmail.com>
2023-03-19 20:18:41 +0100

    Added Author and Tag Models, Using factory faker

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,16 +1,17 @@
-@extends("layout") <!-- Extend layout where repeting lines are located-->
-@section("title")<!-- This section right now is just used for a title -->
-    This is a section post
-@endsection
+@extends("layout") <!-- Extend layout where repeating lines are located-->
+
 
 @section("default_section")<!-- extend in layout after section mark "default section" -->
 <article>
     <!-- Load the title od the post. --> 
-    <h1>{{ $id_post->title }}</h1>
+    <h1 class="posttitle">{{ $id_post->title }}</h1>
     <div>
-        <!-- Load the context od the post. --> 
-         {!! $id_post->body !!}
+    <a class="taglink" href="/tags/{{$id_post->tag->name}}"><div class="tag"><strong>{{$id_post->tag->name}}</strong></div></a>  <!-- Tag section -->
+    <p></p>
+    <!-- Load the context od the post. --> 
+    <p class=postbody>{!! $id_post->body !!}</p>
     </div>
+<p><strong> by <a href="/authors/{{$id_post->author->username}}">{{$id_post->author->name}}</a></strong></p> <!-- Author section -->
 </article>
 <!-- A way to get back to main page intuitively-->
 <a class="backarrow" href="/"><--</a>
